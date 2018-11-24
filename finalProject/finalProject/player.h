4f2bd34d12ce7177b6732fe988b74c4986fd2aa8
christopher-maby <45296266+christopher-maby@users.noreply.github.com>
2018-11-24 13:29:01 -0500

    Update player.h

--- finalProject/finalProject/player.h
+++ finalProject/finalProject/player.h
@@ -1,16 +1,46 @@
-#pragma once
-#include <iostream>
+#ifndef PLAYER_H
+#define PLAYER_H
 #include <string>
-using std::string;
-class Player {
+#include <iostream>
 
-public:
-	string getName();
-	void setActive(bool);
-	bool isActive();
-	int getNRubies();
-	void addReward(const Reward&); 
-	void setDisplayMode(bool endOfGame);
-	Side Player::getSide();
-	void Player::getSide(Side);
+
+ 
+using std::string;
+using std::endl;
+using std::cout;
+enum class Side{defaultplayer=0, top,bottom,left,right};
+
+inline std::ostream& operator<<(std::ostream& out, Side f) {//output the enum how we want it to output the right stuff
+    switch (f) {
+        case Side::defaultplayer: out << "defaultplayer"; break;
+        case Side::top: out << "top"; break;
+        case Side::bottom: out << "bottom"; break;
+        case Side::left: out<< "left"; break;
+        case Side::right:out << "right"; break;
+        default: out << int(f); break;
+    }
+    return out;
+}
+class Player{
+	
+	private:
+		string name;
+		bool active;
+		int rubies;
+		Side plyrSde;
+	public:
+		Player();
+		Player(string name, bool activ, int rubies, Side playerside);
+		string getName() const;
+		void setActive(bool act);
+		bool isActive() const;
+		int getNrubies() const;
+	//	void addReward(const Reward& );
+		Side getSide() const;
+		void setSide(Side);	
 };
+
+
+
+
+#endif
