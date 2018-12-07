1b05aa82120ebac28b0885bbef606f3a189940c1
christopher-maby <45296266+christopher-maby@users.noreply.github.com>
2018-12-07 11:07:47 -0500

    Update player.h

--- finalProject/finalProject/player.h
+++ finalProject/finalProject/player.h
@@ -2,45 +2,44 @@
 #define PLAYER_H
 #include <string>
 #include <iostream>
-
+#include "reward.h"
 
  
 using std::string;
 using std::endl;
 using std::cout;
-enum class Side{defaultplayer=0, top,bottom,left,right};
 
-inline std::ostream& operator<<(std::ostream& out, Side f) {//output the enum how we want it to output the right stuff
-    switch (f) {
-        case Side::defaultplayer: out << "defaultplayer"; break;
-        case Side::top: out << "top"; break;
-        case Side::bottom: out << "bottom"; break;
-        case Side::left: out<< "left"; break;
-        case Side::right:out << "right"; break;
-        default: out << int(f); break;
-    }
-    return out;
-}
+enum class Side{top=0,bottom,left,right};
+
+
+
 class Player{
-	
-	private:
-		string name;
-		bool active;
-		int rubies;
-		Side plyrSde;
+	public:
+	//enum class Side{defaultplayer=0, top=1,bottom=2,left=3,right=4};// this to enumerate each type of player
+		//some other shit here
+		private:
+	string name;
+	bool active;
+	int rubies;
+	Side plyrSde;
 	public:
 		Player();
 		Player(string name, bool activ, int rubies, Side playerside);
-		string getName() const;
-		void setActive(bool act);
-		bool isActive() const;
-		int getNrubies() const;
-	//	void addReward(const Reward& );
-		Side getSide() const;
-		void setSide(Side);	
+		
+		
+	friend std::ostream& operator<<( std::ostream&, const Player&);	
+	//friend std::ostream& operator<<(std::ostream& out, Side& f);
+		
+	string getName() const;
+	void setActive(bool act);
+	bool isActive() const;
+	int getNrubies() const;
+	void addReward(const Reward& r);
+	Side getSide() const;
+	void setSide(Side side);
+	
+
+	
+	
+	
 };
-
-
-
-
-#endif
